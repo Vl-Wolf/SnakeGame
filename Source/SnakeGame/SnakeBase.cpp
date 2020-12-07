4ce82518c6e17cf4b6fbb30e24868601da117a3d
Vladislav Volkov <vlad.volkov.123456789@gmail.com>
2020-12-07 21:05:04 +0300

    Added C++ Classes of Pawn, Snake and SnakeElem

---
+++ Source/SnakeGame/SnakeBase.cpp
@@ -0,0 +1,64 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SnakeBase.h"
+#include "SnakeElementBase.h"
+// Sets default values
+ASnakeBase::ASnakeBase()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	ElementSize = 100.f;
+	MovementSpeed = 10.f;
+	LastMoveDirection = EMovementDirection::DOWN;
+}
+
+// Called when the game starts or when spawned
+void ASnakeBase::BeginPlay()
+{
+	Super::BeginPlay();
+	AddSnakeElement(4);
+}
+
+// Called every frame
+void ASnakeBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	Move(DeltaTime);
+}
+
+void ASnakeBase::AddSnakeElement(int ElementsNum)
+{
+	for (int i = 0; i < ElementsNum; ++i)
+	{
+		FVector NewLocation(SnakeElements.Num() * ElementSize, 0, 0);
+		FTransform NewTransform(NewLocation);
+		ASnakeElementBase* NewSnakeElem = GetWorld()->SpawnActor<ASnakeElementBase>(SnakeElementClass, NewTransform);
+		NewSnakeElem->AttachToActor(this, FAttachmentTransformRules::KeepRelativeTransform);
+		SnakeElements.Add(NewSnakeElem);
+	}
+}
+
+void ASnakeBase::Move(float DeltaTime)
+{
+	FVector MovementVector;
+	float MovementSpeedDelta = MovementSpeed * DeltaTime;
+
+	switch (LastMoveDirection)
+	{
+	case EMovementDirection::UP:
+		MovementVector.X += MovementSpeedDelta;
+		break;
+	case EMovementDirection::DOWN:
+		MovementVector.X -= MovementSpeedDelta;
+		break;
+	case EMovementDirection::LEFT:
+		MovementVector.Y += MovementSpeedDelta;
+		break;
+	case EMovementDirection::RIGHT:
+		MovementVector.Y -= MovementSpeedDelta;
+		break;
+	}
+
+	AddActorWorldOffset(MovementVector);
+}
